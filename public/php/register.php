7d36d2fd2ceef3bbce7113170b84b88a37e32731
ProjektSklepInternetowyPHP <bazydanych770869@gmail.com>
2025-05-28 09:28:13 +0200

    test2

--- public/php/register.php
+++ public/php/register.php
@@ -1,3 +1,27 @@
 <?php
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zarejestruj się</title>
+</head>
+<body>
+    
+
+    <h1>Zarejestruj się</h1>
+
+
+
+    <form action="register.php" method="POST">
+
+
+
+    </form>
+
+
+</body>
+</html>
