1ee87084fe305e4b79d8a551cad9383df504b033
McYeenfg <mcyeenfg@gmail.com>
2025-05-28 09:37:22 +0200

    headerfooter

---
+++ iclude/footer.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <footer style="background: #2e8b57; color: white; padding: 20px; text-align: center; margin-top: 40px;">
+        <p>&copy; <?php echo date('Y'); ?> Sklep Ogrodniczy "Złoty SSman"</p>
+        <p>Adres: polna 7,Warszawa</p>
+        <p>Telefon: +48 *** *** *** | Email: polna@gmailcom[nie pisz]</p>
+        <p>Data ostatniego działania: 28.05.2025</p>
+        <p><a href="regulamin.php" style="color: #fff; text-decoration: underline;">Polityka prywatności</a></p>
+    </footer>
+</body>
+</html>
---
+++ iclude/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <img src="" alt="Złoty SSman">
+        <div class="linki">
+            <a href="/public/php/index.php">Strona główna</a>
+            <a href="/public/php/meble.php">Meble ogrodowe</a>
+            <a href="/public/php/akcesoria.php">Akcesoria</a>
+            <a href="/public/php/rosliny.php">Kwiatki</a>
+            <a href="/public/php/regulamin.php">Regulamin</a>
+        </div>
+        <div class="pole">
+            <textarea name="wyszukaj" id="wyszukaj"></textarea>
+        </div>
+    </header>
+</body>
+</html>
